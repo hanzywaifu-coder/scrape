ddd43ab5a8d134c41e1b2c8ddffabfae88dcbc5a
Buffy <buffy@freebuff.local>
2026-09-12 06:48:12 +0100

    update header: Kumpulan scraper by Dongtube, Scraper manwhaku.my.id: search, detail, download chapter

--- manwhaku.js
+++ manwhaku.js
@@ -3,7 +3,9 @@ const fs = require('fs');
 const path = require('path');
 
 const BASE = 'https://manwhaku.my.id';
-const UA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.4 Safari/605.1.15';
+
+// Kumpulan scraper by Dongtube
+// Scraper manwhaku.my.id: search, detail, download chapter
 
 function httpGet(url, redirect = true) {
   return new Promise((resolve, reject) => {
